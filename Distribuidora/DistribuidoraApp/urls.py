63d154f2cf415eaf44f3fd6945b5097309890a7d
Enzo Almada <enzoalmada30@gmail.com>
2022-07-21 20:38:08 -0300

    Agrega en views register y su template

--- Distribuidora/DistribuidoraApp/urls.py
+++ Distribuidora/DistribuidoraApp/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     # URLS de la App
     path('', inicio, name="inicio"),
+    path('register', register_request, name="register"),
 
 ]
--- Distribuidora/DistribuidoraApp/views.py
+++ Distribuidora/DistribuidoraApp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect,render
+from .forms import *
+from django.contrib.auth import login, logout, authenticate
+
 
 # Create your views here.
 
@@ -6,3 +9,32 @@ from django.shortcuts import render
 def inicio(request):
 
         return render(request,"DistribuidoraApp/index.html",{})
+
+def register_request(request):
+
+    if request.method == "POST":
+        
+        # form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
+
+        if form.is_valid():
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1') # es la primer contraseña, no la confirmacion
+
+            form.save() # registramos el usuario
+            # iniciamos la sesion
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect("inicio")
+            else:
+                return redirect("login")
+
+        return render(request,"DistribuidoraApp/register.html",{"form":form})
+
+    # form = UserCreationForm()
+    form = UserRegisterForm()
+
+    return render(request,"DistribuidoraApp/register.html",{"form":form})
