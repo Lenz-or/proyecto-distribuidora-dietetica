dddd38afe292d7063ec132e8adc0892c524d6920
Enzo Almada <enzoalmada30@gmail.com>
2022-07-21 21:01:27 -0300

    Agrega funcionalidades del login en views

--- Distribuidora/DistribuidoraApp/urls.py
+++ Distribuidora/DistribuidoraApp/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     # URLS de la App
     path('', inicio, name="inicio"),
+    path('login/', login_request, name="login"),
     path('register', register_request, name="register"),
 
 ]
--- Distribuidora/DistribuidoraApp/views.py
+++ Distribuidora/DistribuidoraApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import redirect,render
 from .forms import *
 from django.contrib.auth import login, logout, authenticate
 
+from django.contrib.auth.forms import AuthenticationForm
+
 
 # Create your views here.
 
@@ -9,6 +11,31 @@ from django.contrib.auth import login, logout, authenticate
 def inicio(request):
 
         return render(request,"DistribuidoraApp/index.html",{})
+
+
+def login_request(request):
+
+    if request.method == "POST":
+
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect("inicio")
+            else:
+                return redirect("login")
+        else:
+            return redirect("login")
+    
+    form = AuthenticationForm()
+
+    return render(request,"DistribuidoraApp/login.html",{"form":form})
 
 def register_request(request):
 
